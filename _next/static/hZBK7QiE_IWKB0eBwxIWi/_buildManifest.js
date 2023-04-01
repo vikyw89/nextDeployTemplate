8fb8d3faf0dacbb2756d62f0c321b7e6373dc6a8
vikyw89 <vikyw89@users.noreply.github.com>
2023-04-01 06:49:55 +0000

    deploy: c2b2c054bd6d6416a83e91117462f5ce6e28eb49

---
+++ _next/static/hZBK7QiE_IWKB0eBwxIWi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/457ee4102c2cdd0e.css","static/chunks/pages/index-0ab584cebbcedbc1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
