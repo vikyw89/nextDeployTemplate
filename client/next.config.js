3e1217a2e61ccaee11ab7400e8f9d41c3f380305
vikyw <vikyw89@gmail.com>
2023-04-01 15:24:03 +0800

    input env secret to gh action

--- client/next.config.js
+++ client/next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
   },
   trailingSlash:true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  env:{
-    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH
-  }
 }
 
 module.exports = nextConfig
